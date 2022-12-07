59d034d856c054bd868c53060e482433ce5e6b80
ZhoYev <zholob0213@gmail.com>
2022-12-07 17:59:32 +0000

    add clock(js)

---
+++ JS/clock.js
@@ -0,0 +1,8 @@
+//IIFE
+(() => {
+    function updateClock() {
+        const clockContainer = document.querySelector(".clock");
+        clockContainer.innerText = new Date().toLocaleTimeString();
+    }
+   setInterval(updateClock, 1000);
+}) ();
